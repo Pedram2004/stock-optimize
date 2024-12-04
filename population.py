5b803361a7a45959d04027246f3e055e10da84e0
Pedram Alizadeh <83alizadeh13ped@gmail.com>
2024-12-04 18:43:07 +0330

    mutation and crossover functions added to Population __lt__ and __eq__ added to Vector an attribute and property in Vector renamed

--- population.py
+++ population.py
@@ -10,6 +10,27 @@ class Population:
     @staticmethod
     def __creating_individual() -> Vector:
         random_vector = np.random.uniform(size=Vector.len())
-        adjusted_vector = np.divide(random_vector, np.sum(random_vector))
-        return Vector(adjusted_vector)
+        return Vector(random_vector)
 
+    @staticmethod
+    def __cross_over(vector1: Vector, vector2: Vector) -> list[Vector]:
+        lookup_dict = {0: vector1.values, 1: vector2.values}
+        children: list[list] = [[], []]
+
+        for i in range(Vector.len()):
+            random_num = np.random.randint(low=0, high=2, size=1)  # it is one or zero
+            for j in range(2):
+                children[j].append(lookup_dict.get((random_num + j) % 2)[i])
+
+        return [Vector(np.array(child)) for child in children]
+
+    @staticmethod
+    def __mutate(vector: Vector) -> None:
+        values = vector.values
+        num_mutations = np.ceil(abs(np.random.normal(size=1)))
+        mutation_value = np.random.normal(size=1)
+        for i in range(num_mutations):
+            rand_int = np.random.randint(low=0, high=Vector.len(), size=1)
+            values[rand_int] += mutation_value
+
+        vector.values = values
--- vector.py
+++ vector.py
@@ -1,17 +1,33 @@
 import numpy as np
-from typing import Iterable
+
 
 class Vector:
     __covariance_matrix: np.array
     __expected_return: np.array
 
-    def __init__(self, vector: Iterable):
-        self.__vector = np.array(vector)
+    def __init__(self, vector: np.array):
+        self.__values = np.divide(vector, np.sum(vector))
         self.__fitness = self.__fitness_func()
 
+    def __eq__(self, other):
+        if isinstance(other, Vector):
+            return self.__values == other.__values
+
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, Vector):
+            return self.__fitness < other.__fitness
+
+        return False
+
     @property
-    def vector(self) -> np.array:
-        return np.copy(self.__vector)
+    def values(self) -> np.array:
+        return np.copy(self.__values)
+
+    @values.setter
+    def values(self, vector: np.array):
+        self.__values = np.divide(vector, np.sum(vector))
 
     @property
     def fitness(self) -> float:
@@ -28,8 +44,8 @@ class Vector:
 
     def __fitness_func(self) -> float:
         portfolio_risk = np.sqrt(np.matmul(
-            np.matmul(self.__vector, Vector.__covariance_matrix),
-            self.__vector)[0])
+            np.matmul(self.__values, Vector.__covariance_matrix),
+            self.__values)[0])
 
-        portfolio_return = np.matmul(self.__vector, Vector.__expected_return)[0]
+        portfolio_return = np.matmul(self.__values, Vector.__expected_return)[0]
         return portfolio_return / portfolio_risk
